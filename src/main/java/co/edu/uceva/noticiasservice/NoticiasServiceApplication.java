9f37fd87b8aa8759602d84a41c8414cf06b56a04
adria <adrian007HD@gmail.com>
2024-09-25 23:57:42 -0500

    primer commit

---
+++ src/main/java/co/edu/uceva/noticiasservice/NoticiasServiceApplication.java
@@ -0,0 +1,14 @@
+package co.edu.uceva.noticiasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasServiceApplication.class, args);
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/co/edu/uceva/noticiasservice/NoticiasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.noticiasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
